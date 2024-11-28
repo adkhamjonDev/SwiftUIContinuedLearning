da34baa882e41edfcb4f152db2036cd2a0a324dd
adkhamjon <adkhamjon.rakhimov.dev@gmail.com>
2024-11-28 14:30:25 +0500

    roation gesture

--- SwiftUIContinuedLearning/RotateGesture.swift
+++ SwiftUIContinuedLearning/RotateGesture.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct RotateGesture: View {
+    
+    @State private var angle: Angle = Angle(degrees: 0)
+    
     var body: some View {
         Text("Hello, World!")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .foregroundColor(.white)
+            .padding(50)
+            .background(Color.blue.cornerRadius(10))
+            .rotationEffect(angle)
+            .gesture(
+                RotationGesture()
+                    .onChanged { value in
+                        angle = value
+                    }
+                    .onEnded { value in
+                        withAnimation(.spring()) {
+                            angle = Angle(degrees: 0)
+                        }
+                    }
+            )
     }
 }
 
