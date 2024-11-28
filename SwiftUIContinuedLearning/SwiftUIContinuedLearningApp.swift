bbf3a73da74a3f6faf3d700095c204030ff1acaa
adkhamjon <adkhamjon.rakhimov.dev@gmail.com>
2024-11-28 12:43:25 +0500

    Initial Commit

---
+++ SwiftUIContinuedLearning/SwiftUIContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIContinuedLearningApp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by Adkhamjon Rakhimov on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
